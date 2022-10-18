55acd7f3ba047f440930be9a310040f63cb8dcaf
Vinod Kumar <webindianlion@gmail.com>
2022-10-18 17:29:54 +0530

    First Commit
    First Commit

---
+++ build/precache-manifest.af768690e842e252c33965b1c826035e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba65573e74b2fee4990afaaacd4138d",
+    "url": "/webindianlion/react-website-porfolio/index.html"
+  },
+  {
+    "revision": "181b18944790e47e84c6",
+    "url": "/webindianlion/react-website-porfolio/static/css/main.d0acee29.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/webindianlion/react-website-porfolio/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/webindianlion/react-website-porfolio/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "181b18944790e47e84c6",
+    "url": "/webindianlion/react-website-porfolio/static/js/main.f7e5818e.chunk.js"
+  },
+  {
+    "revision": "3757bef2e2e49209e4f2",
+    "url": "/webindianlion/react-website-porfolio/static/js/runtime-main.1727a903.js"
+  },
+  {
+    "revision": "680982e6aa58367e124d460071324e6f",
+    "url": "/webindianlion/react-website-porfolio/static/media/naafi_photo.680982e6.png"
+  }
+]);
